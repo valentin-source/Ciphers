73bc9c3ac003508fba370b7748e52915f80f40b1
Valentin Buttner <valentin.buttner@gmail.com>
2021-12-02 16:10:12 +0100

    Cesar now implemented, still needs testing though

--- src/Cesar.java
+++ src/Cesar.java
@@ -1,2 +1,33 @@
 public class Cesar {
+
+    static char[] a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,:;!\"§$%&/()=? ".toCharArray();
+
+    public static String encrypt(String input, int key){
+
+        char[] c = input.toCharArray();
+        char[] r = new char[c.length];
+
+        for(int i = 0; i<c.length; i++){
+            int tmp = getCharIndex(c[i]);
+            r[i] = a[Math.abs(((tmp+key)%a.length))];
+        }
+        return charArrayToString(r);
+    }
+
+    public static int getCharIndex(char x){
+        for(int i = 0; i<a.length; i++){
+            if(a[i] == x) return i;
+        }
+        return 0;
+    }
+
+    //converts a char array to a String
+    public static String charArrayToString(char[] c){
+        StringBuilder stringBuilder = new StringBuilder();
+        for(char i : c){
+            stringBuilder.append(i);
+        }
+        return stringBuilder.toString();
+    }
+
 }
--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-        testXOR("Hallo Du Da", "00100100100111111111010010000101000100000100000000111010110101010100010011");
+       // testXOR("Hallo Du Da", "00100100100111111111010010000101000100000100000000111010110101010100010011");
+        testCesar("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!§$%&/()=? ", 1);
     }
 
 
@@ -12,6 +13,16 @@ public class Test {
         System.out.println("Encrypted: " + c);
         c = XOR.encrypt(c, key);
         System.out.println("Decrypted: " + c);
+    }
+
+    public static void testCesar(String input, int key){
+
+        String tmp = Cesar.encrypt(input, key);
+        System.out.println(tmp);
+        tmp = Cesar.encrypt(tmp, Cesar.a.length-key);
+        System.out.println(tmp);
+
+
     }
 
 }
