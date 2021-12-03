35c8ad6c0e5b3360acabc79dd4ddd0b5532cbde7
Valentin Buttner <valentin.buttner@gmail.com>
2021-12-03 10:19:23 +0100

    Changed Name, changed Tests, changed array copy method

---
+++ src/CesarFeistel.java
@@ -0,0 +1,66 @@
+public class CesarFeistel {
+
+    //aAbBcCdD
+    //iC`g'e(
+    private static String s = ""; //needs to be even!!!
+    private static char[] c = s.toCharArray();
+    private static int key = 1;
+    private static int b;  //index of first char of second block
+
+    private static char[] b1;
+    private static char[] b2;
+
+    //temporäre blöcke zum switchen
+    private static char[] b3;
+
+    public static String performFeistel(String Stringin, int keyin){
+
+        if(Stringin.length()%2 != 0) Stringin += " ";
+
+        s = Stringin;
+        c = s.toCharArray();
+        key = keyin;
+
+        b = c.length/2;
+        b1  = new char[c.length/2];
+        b2 = new char[c.length/2];
+        b3 = new char[c.length/2];
+
+        for(int i = 0; i<4; i++){
+            FeistelLoop();
+        }
+
+        //Swapped from orig. implementation in loop
+        //generate 1st block
+        if (b >= 0) System.arraycopy(c, 0, b2, 0, b);
+        //generate 2nd Block
+        if (c.length - b >= 0) System.arraycopy(c, b, b1, 0, c.length - b);
+
+
+        s = Cesar.charArrayToString(b1)+Cesar.charArrayToString(b2);
+        c = s.toCharArray();
+
+        return s;
+
+    }
+
+    private static void FeistelLoop(){
+
+        //generate 1st Block
+        if (b >= 0) System.arraycopy(c, 0, b1, 0, b);
+        //generate 2nd Block
+        if (c.length - b >= 0) System.arraycopy(c, b, b2, 0, c.length - b);
+
+        //create cesar ciphertext from 2nd Block and store it in b3
+        b3 = b1;
+        b1 = b2;//Cesar.encrypt(Cesar.charArrayToString(b2), key).toCharArray();
+        b2 = XOR.encrypt(b3, Cesar.encrypt(Cesar.charArrayToString(b2), key).toCharArray()); //check if correct
+
+        s = Cesar.charArrayToString(b1)+Cesar.charArrayToString(b2);
+        c = s.toCharArray();
+    }
+
+
+
+
+}
--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,13 @@
 public class Test {
     public static void main(String[] args) {
-       // testXOR("Hallo Du Da", "00100100100111111111010010000101000100000100000000111010110101010100010011");
-       // testCesar("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!§$%&/()=? ", 1);
+        // testXOR("Hallo Du Da", "00100100100111111111010010000101000100000100000000111010110101010100010011");
+         testFeistel("Feistel Netzwerk mit Cesar Chiffre:)", 1000000);
+        /*String tmp = "pruefung";
+        for(int i = 0; i<13; i++){
+            tmp = Cesar.encrypt(tmp, 2);
+            System.out.println(tmp);
+        }
+        System.out.println("Result: " + tmp);*/
     }
 
     public static void testXOR(String input, String key){
@@ -23,4 +29,21 @@ public class Test {
 
     }
 
+    public static void testFeistel(String s, int k){
+
+        System.out.println();
+
+        System.out.println("String: " + s);
+        System.out.println("Key: " + k);
+
+        System.out.println("\nEncrypted: ");
+        String tmp = CesarFeistel.performFeistel(s,k);
+        System.out.print("");
+
+        System.out.println(tmp);
+        System.out.println("\nDecrypted: ");
+        tmp = CesarFeistel.performFeistel(tmp, k);
+        System.out.println(tmp);
+
+    }
 }
